37b12cf061df15ed21886bbb1f8e792abbf95131
MasaidFairus <masaidfairustrimarsongko@gmail.com>
2025-05-19 14:16:30 +0700

    Add liveware

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -24,6 +24,7 @@ class AdminPanelProvider extends PanelProvider
     {
         return $panel
             ->default()
+            ->brandName('PIONS Admin Panel')
             ->id('admin')
             ->path('admin')
             ->login()
