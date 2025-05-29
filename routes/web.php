d95b29c7dfec99d13ded5b2668dfa37f2fadc54a
andromeda <q.a.r.h.hrp0204@gmail.com>
2025-05-29 10:35:50 +0700

    partials added

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 Route::get('/login', function () {
