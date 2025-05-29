b7de522bc863fb499e11887dd2b464e405f15af9
andromeda <q.a.r.h.hrp0204@gmail.com>
2025-05-29 10:43:01 +0700

    Footer and Navbar

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,46 @@
+<footer class="w-full h-auto bg-white relative overflow-hidden flex flex-col justify-start items-center">
+    <div class="w-full h-auto py-15 px-10 flex justify-between items-center">
+        <div class="w-auto h-auto flex flex-col justify-center items-start">
+            <img src="assets/logo_pions_colored.svg" class="w-20 h-auto max-w-full mb-2">
+            <h1 class="text-blue-800 text-4xl font-bold text-left capitalize mb-6">pamijahan idn organization <br>nimbly student</h1>
+            <p class="text-blue-800 text-md font-medium text-left uppercase">pions, 2025</p>
+        </div>
+        <div class="w-auto h-auto gap-15 flex justify-end items-center">
+            <div class="w-auto h-auto gap-5 flex flex-col justify-center items-start">
+                <a href="#" class="text-blue-800 text-md font-bold decoration-0 capitalize">dahboard</a>
+                <a href="#" class="text-blue-800 text-md font-bold decoration-0 capitalize">leader of pions</a>
+                <a href="#" class="text-blue-800 text-md font-bold decoration-0 capitalize">what is pions</a>
+                <a href="#" class="text-blue-800 text-md font-bold decoration-0 capitalize">administration</a>
+            </div>
+
+            <div class="w-auto h-auto gap-5 flex flex-col justify-center items-start">
+                <a href="#" class="text-blue-800 text-md font-bold decoration-0 capitalize">organization</a>
+                <a href="#" class="text-blue-800 text-md font-bold decoration-0 capitalize">blog</a>
+                <a href="#" class="text-blue-800 text-md font-bold decoration-0 capitalize">pions</a>
+                <a href="#" class="text-blue-800 text-md font-bold decoration-0 capitalize">news</a>
+            </div>
+
+            <div class="w-auto h-auto gap-5 flex flex-col justify-center items-start">
+                <a href="#" class="text-blue-800 text-md font-bold decoration-0 capitalize">resources</a>
+                <a href="#" class="text-blue-800 text-md font-bold decoration-0 capitalize">documentation</a>
+                <a href="#" class="text-blue-800 text-md font-bold decoration-0 capitalize">gallery</a>
+                <a href="#" class="text-blue-800 text-md font-bold decoration-0 capitalize">event</a>
+            </div>
+
+            <div class="w-auto h-auto gap-5 flex flex-col justify-center items-start">
+                <a href="#" class="text-blue-800 text-md font-bold decoration-0 capitalize">social media</a>
+                <a href="#" class="text-blue-800 text-md font-bold decoration-0 capitalize"><i class="fa-brands fa-instagram mr-1"></i>instagram</a>
+                <a href="#" class="text-blue-800 text-md font-bold decoration-0 capitalize"><i class="fa-brands fa-linkedin mr-1"></i>linkedIn</a>
+                <a href="#" class="text-blue-800 text-md font-bold decoration-0 capitalize"><i class="fa-brands fa-github mr-1"></i>github</a>
+            </div>
+        </div>
+    </div>
+    <div class="w-full h-auto py-5 px-18 bg-blue-800 flex justify-between items-center">
+        <p class="text-white text-md font-medium">@ 2025 PIONS . All rights reserved.</p>
+        <div class="w-auto h-auto gap-10 flex justify-center items-center">
+            <a href="#" class="text-white text-md font-medium decoration-0 capitalize">terms of services</a>
+            <a href="#" class="text-white text-md font-medium decoration-0 capitalize">privacy policy</a>
+            <a href="#" class="text-white text-md font-medium decoration-0 capitalize">cookies</a>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,32 @@
+<nav class="w-full h-auto py-4 bg-blue-800/10 backdrop-blur-sm fixed top-0 flex justify-center items-center z-20">
+    <img src="assets/logo_pions.svg" class="w-20 h-auto max-w-full mr-50 invisible lg:visible">
+    <div class="w-auto h-auto mr-20 px-10 py-1 bg-white shadow-sm shadow-black/50 rounded-full gap-5 flex justify-center items-center invisible lg:visible">
+        <a href="#" class="px-6 py-2 rounded-md text-md text-black font-normal capitalize">home</a>
+        <a href="#" class="px-6 py-2 rounded-md text-md text-black font-normal capitalize">register</a>
+        <a href="#" class="px-6 py-2 rounded-md text-md text-black font-normal capitalize">about us</a>
+        <a href="#" class="px-6 py-2 rounded-md text-md text-black font-normal capitalize">contact us</a>
+    </div>
+    <div class="w-auto h-auto gap-5 flex justify-center items-center invisible lg:visible">
+        <a href="#" class="px-8 py-3 border-4 border-blue-950 rounded-full bg-blue-950 hover:bg-transparent text-md shadow-sm shadow-black/50 text-white font-medium capitalize transition-all">login</a>
+        <a href="#" class="px-8 py-3 border-4 border-blue-600 rounded-full bg-blue-600 hover:bg-transparent text-md shadow-sm shadow-black/50 text-white font-medium capitalize transition-all">register</a>
+    </div>
+    <i onclick="responsif()" class="fa-solid fa-bars text-2xl py-3 px-3 rounded-lg bg-white text-blue-800 cursor-pointer absolute right-5 visible lg:invisible"></i>
+</nav>
+
+<div id="mobile" class="w-auto h-screen px-5 py-13 bg-blue-800/55 backdrop-blur-xs fixed right-0 top-0 flex flex-col justify-start items-center z-30 transition-all translate-x-full">
+    <div class="w-full h-auto mb-10 flex justify-between items-center">
+        <img src="assets/logo_pions.svg" class="w-13 h-auto max-w-full">
+        <i onclick="web()" class="fa-solid fa-xmark text-2xl py-3 px-3 rounded-lg bg-white text-blue-800 cursor-pointer"></i>
+    </div>
+    <div class="w-full h-auto mb-10 gap-3 flex flex-col justify-center items-start">
+        <a href="#" class="text-lg text-black w-full h-auto px-3 py-2 bg-white font-medium decoration-0 rounded-md capitalize">Home</a>
+        <a href="#" class="text-lg text-black w-full h-auto px-3 py-2 bg-white font-medium decoration-0 rounded-md capitalize">About Us</a>
+        <a href="#" class="text-lg text-black w-full h-auto px-3 py-2 bg-white font-medium decoration-0 rounded-md capitalize">Services</a>
+        <a href="#" class="text-lg text-black w-full h-auto px-3 py-2 bg-white font-medium decoration-0 rounded-md capitalize">Doctors</a>
+        <a href="#" class="text-lg text-black w-full h-auto px-3 py-2 bg-white font-medium decoration-0 rounded-md capitalize">FaQ</a>
+    </div>
+    <div class="w-full h-auto gap-5 flex justify-start items-center">
+        <a href="#" class="px-10 py-2 border-4 border-blue-950 rounded-full bg-blue-950 hover:bg-transparent text-md shadow-sm shadow-black/50 text-white font-medium capitalize transition-all">login</a>
+        <a href="#" class="px-10 py-2 border-4 border-blue-600 rounded-full bg-blue-600 hover:bg-transparent text-md shadow-sm shadow-black/50 text-white font-medium capitalize transition-all">register</a>
+    </div>
+</div>
