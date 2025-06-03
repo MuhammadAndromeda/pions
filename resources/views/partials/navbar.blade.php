ab400e95ecd78210aedaace2c4246f84e54794e9
fadhelrpl <mohamadfadhelrpl@gmail.com>
2025-06-03 10:13:46 +0700

    Fix navbar one more

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -5,6 +5,8 @@
     <div
         class="w-auto h-auto mr-20 px-10 py-1 bg-white shadow-sm shadow-black/50 rounded-full gap-5 flex justify-center items-center invisible lg:visible">
         <a href="{{ route('home') }}" class="px-6 py-2 rounded-md text-md text-black font-normal capitalize">home</a>
+        <a href="{{ route('news') }}" class="px-6 py-2 rounded-md text-md text-black font-normal capitalize">news</a>
+        <a href="{{ route('event') }}" class="px-6 py-2 rounded-md text-md text-black font-normal capitalize">event</a>
         <a href="{{ route('apply') }}" class="px-6 py-2 rounded-md text-md text-black font-normal capitalize">apply</a>
 
         {{-- DROPdown About Us (Desktop) --}}
@@ -71,7 +73,11 @@
     <div class="w-full h-auto mb-10 gap-3 flex flex-col justify-center items-start">
         <a href="{{ route('home') }}"
             class="text-lg text-black w-full h-auto px-3 py-2 bg-white font-medium decoration-0 rounded-md capitalize">Home</a>
-        
+        <a href="{{ route('news') }}"
+            class="text-lg text-black w-full h-auto px-3 py-2 bg-white font-medium decoration-0 rounded-md capitalize">News</a>
+        <a href="{{ route('event') }}"
+            class="text-lg text-black w-full h-auto px-3 py-2 bg-white font-medium decoration-0 rounded-md capitalize">Event</a>
+
         <a href="{{ route('apply') }}"
             class="text-lg text-black w-full h-auto px-3 py-2 bg-white font-medium decoration-0 rounded-md capitalize">Apply</a>
 
@@ -80,7 +86,7 @@
             <a href="#" @click.prevent="open = !open">
                 About Us
             </a>
-            
+
             <div x-show="open" x-transition:enter="transition ease-out duration-200"
                 x-transition:enter-start="opacity-0 translate-y-1" x-transition:enter-end="opacity-100 translate-y-0"
                 x-transition:leave="transition ease-in duration-150" x-transition:leave-start="opacity-100 translate-y-0"
